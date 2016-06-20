82aa4ead45cc6bc71a5678bf2cc105fc2f15898b
Gallyamov Vitaly <vitgvr@gmail.com>
2016-06-21 01:59:21 +0600

    save readme and hooks

---
+++ README.md
@@ -0,0 +1,14 @@
+Git Hooks - Для презентации Creative
+=============================
+
+QUICK START
+-----------
+
+On command line, type in the following commands:
+
+        $ cd app
+        $ npm install
+        $ gulp hook
+
+Последняя команда создает символическу ссылку на хук в директорию
+.git/hooks
--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,12 +1,35 @@
 var gulp = require('gulp'),
-	jslint = require('gulp-jslint');
+	jshint = require('gulp-jshint'),
+	map = require('map-stream'),
+	symlink = require('gulp-symlink');
+
+//custom reporter
+var errorReporter = function () {
+  return map(function (file, cb) {
+	if (!file.jshint.success) {
+		process.exit(1);
+	}
+	cb(null, file);
+  });
+};
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-gulp.task('jslint', function(){
+gulp.task('hook', function () {
+	return gulp.src('hooks/.pre-commit')
+		.pipe(symlink(function(file){
+			return new symlink.File({
+				path: '../.git/hooks/pre-commit',
+				cwd: process.cwd()
+			});
+		}, {force: true}));
+});
+
+gulp.task('jshint', function(){
 	return gulp.src('./js/**/*.js')
-		.pipe(jslint())
-		.pipe(jslint.reporter('default'));
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(errorReporter());
 });
